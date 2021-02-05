2c2339f3bd9b05f832945f180026298fdecb1fcf
Stuart Maxwell <stuart@amanzi.nz>
2021-02-05 21:15:02 +1300

    Add pytest

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,6 @@
-from django.test import TestCase
-
-# Create your tests here.
+def test_user_can_authenticate(client, django_user_model):
+    username = "foo1"
+    password = "bar1"
+    user = django_user_model.objects.create_user(username=username, password=password)
+    client.force_login(user)
+    assert user.is_authenticated
